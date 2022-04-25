4eeb4ad3ce29b131df8473cd7e9b1b0e9cc1f6cf
orellanasilvia <104109498+orellanasilvia@users.noreply.github.com>
2022-04-25 19:35:34 -0300

    Initial commit

---
+++ ejercicios-21-4/granjerita2.ts
@@ -0,0 +1,12 @@
+console.log("avanzar");
+console.log("llenar");
+console.log("avanzar");
+console.log("avanzar");
+console.log("avanzar");
+console.log("llenar");
+console.log("avanzar");
+console.log("eliminar");
+console.log("avanzar");
+console.log("eliminar");
+console.log("avanzar");
+console.log("tarea finalizada");
---
+++ ejercicios-21-4/granjerita3.ts
@@ -0,0 +1,9 @@
+console.log("girar a la izquierda");
+console.log("avanzar");
+console.log("girar a la derecha");
+console.log("avanzar");
+console.log("avanzar");
+console.log("girar a la derecha");
+console.log("avanzar");
+console.log("eliminar");
+console.log("tarea finalizada");
